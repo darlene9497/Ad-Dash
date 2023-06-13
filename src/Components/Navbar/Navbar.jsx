ce2fd564a4705c78d8ca1760e45abbe59b6d992c
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-06-13 19:17:17 +0300

    Added and modified some components

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 import './navbar.scss'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 
 export default function Navbar() {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="search..." />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className='icon' />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className='icon' />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className='icon' />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <img src="https://img.freepik.com/free-photo/cartoon-character-with-yellow-jacket-sunglasses_71767-101.jpg?w=740&t=st=1686637439~exp=1686638039~hmac=222c659bd10b77c97d7d21962ab1705feb6b1a745c930c0f4eb7f9510b0f04e0"
+             alt=""
+             className='avatar' />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import Chart from '../../Components/Charts/Chart'
+import Featured from '../../Components/Featured/Featured'
+import Navbar from '../../Components/Navbar/Navbar'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Widget from '../../Components/Widget/Widget'
 import './home.scss'
 
 export default function Home() {
@@ -7,7 +11,17 @@ export default function Home() {
     <div className='home'>
       <Sidebar />
       <div className="homeContainer">
-        container
+        <Navbar />
+        <div className="widgets">
+          <Widget type='user' />
+          <Widget type='order' />
+          <Widget type='earnings' />
+          <Widget type='balance' />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
       </div>
     </div>
   )
