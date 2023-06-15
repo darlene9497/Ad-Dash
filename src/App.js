00805a09a825ddc5c1bb8eeefaff5d8cf2014b06
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-06-15 13:05:29 +0300

    Progress

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function App() {
         <Route path=":userId" element={<Single />} />
         <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
       </Route>
-      <Route path="/products">
+      {/* <Route path="/products">
         <Route index element={<List />} />
         <Route path=":productId" element={<Single />} />
         <Route path="new" element={<New inputs={productInputs} title="Add New Product" />} />
-      </Route>
+      </Route> */}
     </Routes>
     </BrowserRouter>
     </div>
--- src/Pages/Single/Single.jsx
+++ src/Pages/Single/Single.jsx
@@ -1,7 +1,58 @@
 import React from 'react'
+import './single.scss'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import Navbar from '../../Components/Navbar/Navbar'
+import Chart from '../../Components/Charts/Chart'
+import Meza from '../../Components/Table/Table'
 
 export default function Single() {
   return (
-    <div>Single</div>
+    <div className="single">
+    <Sidebar />
+    <div className="singleContainer">
+      <Navbar />
+      <div className="top">
+        <div className="left">
+          <div className="editButton">Edit</div>
+          <h1 className="title">Information</h1>
+          <div className="item">
+            <img
+              src="https://img.freepik.com/free-photo/tiger-leather-jacket-stands-street-front-neon-sign-that-says-i-am-tiger_1340-39347.jpg?size=626&ext=jpg"
+              alt=""
+              className="itemImg"
+            />
+            <div className="details">
+              <h1 className="itemTitle">My Name</h1>
+              <div className="detailItem">
+                <span className="itemKey">Email:</span>
+                <span className="itemValue">myemail@gmail.com</span>
+              </div>
+              <div className="detailItem">
+                <span className="itemKey">Phone:</span>
+                <span className="itemValue">+1 2345 67 89</span>
+              </div>
+              <div className="detailItem">
+                <span className="itemKey">Address:</span>
+                <span className="itemValue">
+                  My Address, Nairobi
+                </span>
+              </div>
+              <div className="detailItem">
+                <span className="itemKey">Country:</span>
+                <span className="itemValue">Kenya</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
+        </div>
+      </div>
+      <div className="bottom">
+      <h1 className="title">Last Transactions</h1>
+        <Meza/>
+      </div>
+    </div>
+  </div>
   )
 }
